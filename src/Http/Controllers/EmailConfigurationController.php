3f4904cfa379e97e712e5d29de768ecb96446456
Sijan Mahato <sijan.mahato@cibnepal.org.np>
2026-05-26 16:02:22 +0545

    fix: chnages

--- src/Http/Controllers/EmailConfigurationController.php
+++ src/Http/Controllers/EmailConfigurationController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Karja\EmailConfig\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
