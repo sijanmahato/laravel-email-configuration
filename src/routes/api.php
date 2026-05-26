a8e0b906b8ea2e1fc8d87beffb6927513ac6ca98
Sijan Mahato <sijan.mahato@cibnepal.org.np>
2026-05-26 15:06:45 +0545

    fix: api changes

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Karja\EmailConfig\Http\Controllers\EmailConfigurationController;
 
-Route::get('/', [EmailConfigurationController::class, 'index']);
-Route::post('/', [EmailConfigurationController::class, 'store']);
-Route::get('{email_configuration}', [EmailConfigurationController::class, 'show']);
-Route::put('{email_configuration}', [EmailConfigurationController::class, 'update']);
-Route::delete('{email_configuration}', [EmailConfigurationController::class, 'destroy']);
-Route::post('{email_configuration}/test-send', [EmailConfigurationController::class, 'testSend']);
+Route::get('/email-configurations', [EmailConfigurationController::class, 'index']);
+Route::post('/email-configurations', [EmailConfigurationController::class, 'store']);
+Route::get('/email-configurations/{email_configuration}', [EmailConfigurationController::class, 'show']);
+Route::put('/email-configurations/{email_configuration}', [EmailConfigurationController::class, 'update']);
+Route::delete('/email-configurations/{email_configuration}', [EmailConfigurationController::class, 'destroy']);
+Route::post('/email-configurations/{email_configuration}/test-send', [EmailConfigurationController::class, 'testSend']);
